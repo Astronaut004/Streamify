ce3d5f0d04020f7cbfbbba1545d977af8a2dd6c6
Astronaut004 <ac4548817@gmail.com>
2024-11-20 01:22:33 +0530

    Add Dropmenu and Sign out tags

--- streamify/src/components/Header.jsx
+++ streamify/src/components/Header.jsx
@@ -14,7 +14,7 @@ import { useEffect } from 'react';
 
 
 function Header(props) {
-    
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const username = useSelector(selectUserName);
@@ -25,11 +25,11 @@ function Header(props) {
         auth.onAuthStateChanged(async (user) => {
             if (user) {
                 setUser(user);
-                navigate("/home"); 
+                navigate("/home");
             }
         });
     }, [username]);
-     
+
 
     const handleAuth = () => {
         signIn()
@@ -86,7 +86,12 @@ function Header(props) {
                                 <span>SERIES</span>
                             </a>
                         </NavMenu>
-                        <UserImg src={userPhoto} alt={username}/>
+                        <SignOut>
+                            <UserImg src={userPhoto} alt={username} />
+                            <DropDown>
+                                <span onClick={handleAuth}>Sign Out</span>
+                            </DropDown>
+                        </SignOut>
                     </>
             }
 
@@ -209,4 +214,43 @@ const UserImg = styled.img`
     height: 100%;
 
 `;
+
+const DropDown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: 0px;
+    background: rgb(19,19,19);
+    border: 1px solid rgb(151,151,151,0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+    padding: 10px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 120px;
+    opacity: 0;
+`;
+
+const SignOut = styled.div`
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg} {
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        ${DropDown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
+    }
+`;
+
 export default Header;
